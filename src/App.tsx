31bdb3ec9e3ff4e6a53b3174e6625df42e4c66cc
Abhishek Saha <84782975+abhisheksaha2002@users.noreply.github.com>
2024-01-09 22:29:41 +0530

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { ToastContainer } from "react-toastify";
+import Table from "./components/table.component";
+
+
+function App() {
+  return (
+    <>
+      {/* <ToastContainer /> */}
+      <Table />
+    </>
+  );
+}
+
+export default App;
